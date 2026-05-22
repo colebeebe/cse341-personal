df9d3e884b4f0494bdfef8af9a12f32c1ed86ac6
colebeebe <bee14003@byui.edu>
2026-05-22 15:47:44 -0400

    Created POST route for movies

--- controllers/movies.js
+++ controllers/movies.js
@@ -2,9 +2,15 @@ import { getDB } from '../models/db.js';
 
 export const getMovies = async (req, res) => {
   const db = getDB();
-
   const collection = db.collection('movies');
   const items = await collection.find({}).toArray();
 
   res.send(items);
+};
+
+export const postMovie = async (req, res) => {
+  const db = getDB();
+  const collection = db.collection('movies');
+  await collection.insertOne(req.body);
+  res.send(req.body);
 };
--- controllers/routes.js
+++ controllers/routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { getMovies } from './movies.js';
+import { getMovies, postMovie } from './movies.js';
 
 const routes = Router();
 
 routes.get('/movies', getMovies);
+routes.post('/movies', postMovie);
 
 export default routes;
